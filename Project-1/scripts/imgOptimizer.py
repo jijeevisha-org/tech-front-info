8697adf8d54babbac852e4a3d7980606d8c5df03
Ravi Prakash <1907prakash.ravi@gmail.com>
2022-12-09 20:55:31 +0530

    processing png

--- Project-1/scripts/imgOptimizer.py
+++ Project-1/scripts/imgOptimizer.py
@@ -1,15 +1,24 @@
 import urllib.request as req
 from PIL import Image
 
-def optimize(imgPath = "featured.jpg", picNameDefault = "featured.jpg", reduceBy = 45):
+def optimize(imgPath = "featured.jpg", picNameDefault = "featured.jpg", reduceBy = 45, sizeOriginal=False):
+  #####################
   # if url is given
   if 'http://' in imgPath or 'https://' in imgPath:
     #imgURL = input("URL of banner image: ")
     _ = req.urlretrieve(imgPath, picNameDefault)
     imgPath = picNameDefault
+  #####################
+  ## check for 'jpg' and convert if needed
+  im = Image.open(imgPath)
+  if imgPath[-4:] == ".png":
+    jpg_im = im.convert('RGB')
+    jpg_im.save(imgPath)
   # optimize
-  currImg = Image.open(imgPath)
+  currImg = Image.open(imgPath) # it can be same as jpg_im
+  #####################
   # downsize the image with an ANTIALIAS filter (gives the highest quality)
-  currImg = currImg.resize((1280,800),Image.ANTIALIAS)
+  newSize = currImg.size if sizeOriginal else (1280,800)
+  currImg = currImg.resize(newSize,Image.ANTIALIAS)
   currImg.save(picNameDefault, optimize=True, quality=reduceBy)
   return
