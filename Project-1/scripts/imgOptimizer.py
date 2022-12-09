ce5dfd0c39ea227928996fdec621be4ba9fcd6c9
Ravi Prakash <1907prakash.ravi@gmail.com>
2022-12-09 10:23:41 +0530

    init

---
+++ Project-1/scripts/imgOptimizer.py
@@ -0,0 +1,15 @@
+import urllib.request as req
+from PIL import Image
+
+def putImg(imgPath = "featured.jpg", picNameDefault = "featured.jpg", reduceBy = 45):
+  # if url is given
+  if 'http://' in imgPath or 'https://' in imgPath:
+    #imgURL = input("URL of banner image: ")
+    _ = req.urlretrieve(imgPath, picNameDefault)
+    imgPath = picNameDefault
+  # optimize
+  currImg = Image.open(imgPath)
+  # downsize the image with an ANTIALIAS filter (gives the highest quality)
+  currImg = currImg.resize(currImg.size,Image.ANTIALIAS)
+  currImg.save(picNameDefault, optimize=True, quality=reduceBy)
+  return
